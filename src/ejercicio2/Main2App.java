bb9597611bbc86ce46b9b24555fcfa7169ba852e
Paula Sousa <paulasousa137@gmail.com>
2023-03-30 19:09:53 +0200

    Ejercicio 2

---
+++ src/ejercicio2/Main2App.java
@@ -0,0 +1,14 @@
+package ejercicio2;
+/*Crea una simple lista de peliculas.Tendremos un JComboBox, donde almacenaremos las películas que vayamos 
+ * almacenando en un campo de texto. Al pulsar el botón Añadir la película que hayamos metido se introducirá
+ *  en el JComboBox.*/
+
+public class Main2App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ej2Frame frame = new ej2Frame();
+	}
+
+}
---
+++ src/ejercicio2/ej2Frame.java
@@ -0,0 +1,83 @@
+package ejercicio2;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import ejercicio1.ejercicio1Info;
+
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class ej2Frame extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField textField;
+
+	JComboBox<String> comboBox_1 = new JComboBox<String>();
+
+	/**
+	 * Create the frame.
+	 */
+	public ej2Frame() {
+		setTitle("Películas");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 496, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		
+		
+		
+		JLabel lblNewLabel = new JLabel("Escribe el título de una película");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblNewLabel.setBounds(45, 73, 196, 33);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Películas");
+		lblNewLabel_1.setBounds(262, 83, 89, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		textField = new JTextField();
+		textField.setBounds(45, 116, 174, 21);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Añadir");
+		btnNewButton.addActionListener(aL);
+			
+		btnNewButton.setBounds(45, 154, 89, 23);
+		contentPane.add(btnNewButton);
+		comboBox_1.setBounds(262, 115, 129, 22);
+		contentPane.add(comboBox_1);
+		
+		setVisible(true);
+	}
+	ActionListener aL = new ActionListener()
+	{
+public void actionPerformed(ActionEvent e) {
+	
+	
+	comboBox_1.addItem(textField.getText());
+	textField.setText(null);
+	textField.requestFocus();
+	
+	
+	
+}
+	};
+}
